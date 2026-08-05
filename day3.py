24d31afa9ff312fd30980f690ded508dd3eba513
nischayshrestha <nikechrestha@gmail.com>
2026-08-05 14:05:43 +0545

    Day-2 of learning pyhon

---
+++ day3.py
@@ -0,0 +1,49 @@
+# list data
+# names = ['Nischay','kcnkjdq']
+# countries = ["japan","usa","nepal"]
+# print(countries[-1])
+# append inside the list
+# countries.append("India")
+# print(countries[-1])
+# removing the last element
+# countries.pop()
+# print(countries)
+
+# adding in any position
+# countries.insert(0,"india")
+# print(countries)
+# countries[1] = "china"
+# print(countries)
+# print(len(countries))
+
+
+# tuple
+# countries_tuple = ("Nepal","Japan","USA","Nepal")
+# countries[2] = "India"
+# print(countries)
+
+# index
+# print(countries_tuple.index("Nepal"))
+
+
+# sets
+
+# countries_sets = {"Nepal","Japan","USA","Nepal"}
+# print(countries_sets)
+
+
+# dictionary
+person_information = {
+    "full_name":"Nischay Shrestha",
+    "address" : "Dharan",
+    "company" : "Digital Pathshala"
+}
+# print(person_information["full_name"])
+
+# person_information["color"] = "blue"
+# print(person_information)
+
+person_information.pop("company")
+print(person_information)
+
+person_information.insert("")
