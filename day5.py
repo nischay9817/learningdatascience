10c58f92db1173b094cd31ef1996ca2dbc7f5653
nischayshrestha <nikechrestha@gmail.com>
2026-08-11 06:04:41 +0545

    learning the function part

---
+++ day5.py
@@ -0,0 +1,31 @@
+# def greet():
+#     print("Hello World")
+# greet()
+
+
+# def greet(name):
+#     print(f"Hello {name}")
+
+# greet("Nischay")
+# greet("Shrestha")
+# greet("Ram")
+
+# countries = ["japan","Nepal","India"]
+# print(len(countries))
+
+# def add(a,b):
+#     return a+b
+
+# sum = add(5,6)
+# print(sum)
+
+# def student():
+#     return "Nischay",22,"Dharan"
+
+# name,age,location = student()
+# print(type(data))
+# print(name)
+
+# lambda function
+# result2 = lambda num:num*num
+# print(result2(2))
