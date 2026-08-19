83b363c98fd9782b7ae6d8816ab9ebd60f0bc602
nischayshrestha <nikechrestha@gmail.com>
2026-08-19 06:04:22 +0545

    more learning of oop

--- oop.py
+++ oop.py
@@ -116,23 +116,148 @@
 
 # create account class with 2 attributes balance and account number. Create methods for debit, credit and printing the balance
 
-class Account:
-    def __init__(self,accountno,balance):
-        self.accountNo = accountno
-        self.balance = balance
+# class Account:
+#     def __init__(self,accountno,balance):
+#         self.accountNo = accountno
+#         self.balance = balance
     
-    def debit(self,deductAmount):
-        self.balance = self.balance - deductAmount  
+#     def debit(self,deductAmount):
+#         self.balance = self.balance - deductAmount  
     
-    def credit(self, inputAmount):
-        self.balance = self.balance + inputAmount
+#     def credit(self, inputAmount):
+#         self.balance = self.balance + inputAmount
     
-    def checkingBalance(self):
-        print(f"Checking current balance {self.balance}")
+#     def checkingBalance(self):
+#         print(f"Checking current balance {self.balance}")
 
-A1 = Account(17874594656,200000)
-A1.checkingBalance()
-A1.credit(100000)
-A1.checkingBalance()
-A1.debit(100000)
-A1.checkingBalance()
+# A1 = Account(17874594656,200000)
+# A1.checkingBalance()
+# A1.credit(100000)
+# A1.checkingBalance()
+# A1.debit(100000)
+# A1.checkingBalance()
+
+# deleting the object
+# class Student:
+#     def __init__(self,name):
+#         self.name = name
+
+# s1 = Student("Nischay")
+# # print(s1)
+
+# del s1
+# print(s1)
+
+# private concept of oop
+# class Account:
+#     def __init__(self, acc_no, acc_pass):
+#         self.acc_no = acc_no
+#         self.__acc_pass = acc_pass
+
+# acc1 = Account("12345","20000")
+# print(acc1.acc_no)
+# print(acc1.__acc_pass)
+
+
+# inheritance
+# class Car:
+#     @staticmethod
+#     def start():
+#         print("Car started...")
+    
+#     @staticmethod
+#     def stop():
+#         print("Car stopped...")
+
+# # inherited:
+# class Toyota(Car):
+#     def __init__(self,name):
+#         self.name = name
+
+# car1 = Toyota("book")
+
+# print(car1.start())
+
+# super() method
+# class Car:
+#     def __init__(self,type):
+#         self.type = type
+
+# class Toyota(Car):
+#     def __init__(self, name, type):
+#         self.name = name
+#         super().__init__(type)
+
+# car1 = Toyota("Fornutnur", "Petrol")
+# print(car1.type)
+
+
+# class Person:
+#     name = "Anonymous"
+#     def changeName(self,name):
+#         Person.name = name
+
+
+# p1 = Person()
+
+# p1.changeName("Nischay")
+
+# print(p1.name)
+# print(Person.name)
+
+
+
+# class method 
+# class Student:
+#     name = "anonymous"
+#     @classmethod
+#     def changeName(cls,name):
+#         cls.name = name
+
+# s1 = Student()
+# s1.changeName("Nischay")
+# print(s1.name)
+# print(Student.name)
+
+# @Property method in python
+# class Student:
+#     def __init__(self, phy, chem, math):
+#         self.phy = phy
+#         self.chem = chem
+#         self.math = math
+    
+#     @property
+#     def percentage(self):
+#         return str((self.phy + self.chem + self.math)/3) + "%"
+
+# stu1 = Student(98,97,99)
+# print(stu1.percentage)
+
+# stu1.phy = 86
+# print(stu1.phy)
+# print(stu1.percentage)
+
+
+# print([1, 2, 3]+[4, 5, 6])
+
+class Complex:
+    def __init__(self, real, img):
+        self.real = real
+        self.img = img
+    
+    def showNumber(self):
+        print(self.real,"i+",self.img,"j")
+    
+    def add(self, num2):
+        newReal = self.real + num2.real
+        newImg = self.img + num2.img
+        return Complex(newReal, newImg)
+
+num1 = Complex(1,5)
+num1.showNumber()
+
+num2 = Complex(4,6)
+num2.showNumber()
+
+num3 = num1.add(num2)
+num3.showNumber()
